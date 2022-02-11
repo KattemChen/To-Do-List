84241b55e1cae9412681e3e16c522af6e0a0357d
KattemChen <1043641135@qq,com>
2022-02-11 11:35:40 +0800

    feat:异步

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
   const [todos, setTodos] = useState([
@@ -15,49 +15,47 @@ function App() {
     {
       content: 'Build a todo app in React',
       isCompleted: false,
-    }
-  ]);
+    },
+  ])
 
   function handleKeyDown(e, i) {
     if (e.key === 'Enter') {
-      createTodoAtIndex(e, i);
+      createTodoAtIndex(e, i)
     }
     if (e.key === 'Backspace' && todos[i].content === '') {
-      e.preventDefault();
-      return removeTodoAtIndex(i);
+      e.preventDefault()
+      return removeTodoAtIndex(i)
     }
   }
 
   function createTodoAtIndex(e, i) {
-    const newTodos = [...todos];
+    const newTodos = [...todos]
     newTodos.splice(i + 1, 0, {
       content: '',
       isCompleted: false,
-    });
-    setTodos(newTodos);
-    setTimeout(() => {
-      document.forms[0].elements[i + 1].focus();
-    }, 0);
+    })
+    setTodos(newTodos)
+    queueMicrotask(() => document.forms[0].elements[i + 1].focus())
   }
 
   function updateTodoAtIndex(e, i) {
-    const newTodos = [...todos];
-    newTodos[i].content = e.target.value;
-    setTodos(newTodos);
+    const newTodos = [...todos]
+    newTodos[i].content = e.target.value
+    setTodos(newTodos)
   }
 
   function removeTodoAtIndex(i) {
-    if (i === 0 && todos.length === 1) return;
-    setTodos(todos => todos.slice(0, i).concat(todos.slice(i + 1, todos.length)));
-    setTimeout(() => {
-      document.forms[0].elements[i - 1].focus();
-    }, 0);
+    if (i === 0 && todos.length === 1) return
+    setTodos((todos) =>
+      todos.slice(0, i).concat(todos.slice(i + 1, todos.length))
+    )
+    queueMicrotask(() => document.forms[0].elements[i - 1].focus())
   }
 
   function toggleTodoCompleteAtIndex(index) {
-    const temporaryTodos = [...todos];
-    temporaryTodos[index].isCompleted = !temporaryTodos[index].isCompleted;
-    setTodos(temporaryTodos);
+    const temporaryTodos = [...todos]
+    temporaryTodos[index].isCompleted = !temporaryTodos[index].isCompleted
+    setTodos(temporaryTodos)
   }
 
   return (
@@ -69,23 +67,23 @@ function App() {
         <ul>
           {todos.map((todo, i) => (
             <div className={`todo ${todo.isCompleted && 'todo-is-completed'}`}>
-              <div className={'checkbox'} onClick={() => toggleTodoCompleteAtIndex(i)}>
-                {todo.isCompleted && (
-                  <span>&#x2714;</span>
-                )}
+              <div
+                className={'checkbox'}
+                onClick={() => toggleTodoCompleteAtIndex(i)}>
+                {todo.isCompleted && <span>&#x2714;</span>}
               </div>
               <input
                 type="text"
                 value={todo.content}
-                onKeyDown={e => handleKeyDown(e, i)}
-                onChange={e => updateTodoAtIndex(e, i)}
+                onKeyDown={(e) => handleKeyDown(e, i)}
+                onChange={(e) => updateTodoAtIndex(e, i)}
               />
             </div>
           ))}
         </ul>
       </form>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
